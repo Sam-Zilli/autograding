c4c46477d1d474f2a6b61d4a43007e985d44a72d
Sam Zilli <samzilli01@gmail.com>
2024-10-13 11:00:36 -0400

    changed to python

--- eval.py
+++ eval.py
@@ -2,18 +2,15 @@ import subprocess
 
 def run_job(cmd):
     ret = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return ret.stdout.decode(('UTF-8'))
-    
-if __name__ == '__main__':
-    # Compile
-    run_job("gcc main.c")
+    return ret.stdout.decode('UTF-8')
 
-    # Run
-    msg = run_job("./a.out").rstrip()
+if __name__ == '__main__':
+    # Run the Python script
+    msg = run_job("python3 script.py").rstrip()  # Change "script.py" to your Python file name
     if msg == "Hello World!":
-        print("Ok!")
+        print("Correct!")
     else:
-        print("Error. The correct answer is `Hello World!`, but your outupt is:")
+        print("Error. The correct answer is `Hello World!`, but your output is:")
         print("```")
         print(msg)
         print("```")
